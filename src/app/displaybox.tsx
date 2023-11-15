b054352720f029951cbb33ec54944be30f1c7e9c
duonghv1 <thuyduong8122002@gmail.com>
2023-11-15 12:36:09 -0800

    added code for fish icon whenever the fish answers

--- src/app/displaybox.tsx
+++ src/app/displaybox.tsx
@@ -13,18 +13,29 @@ export default function DisplayBox({
   const ref = useRef<HTMLSpanElement>(null);
 
   return (
-    <div className="block w-full rounded-md border-0 px-3.5 py-2 bg-white text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 sm:text-sm sm:leading-6">
+    <div className="flex items-center">
       {isQuestion ? (
-        <>{text}</>
+        <></>
       ) : (
-        <TypeAnimation
-          ref={ref}
-          sequence={[`${text}`, () => setLoading(false)]}
-          speed={80}
-          style={{ whiteSpace: "pre-line" }}
-          cursor={false}
+        <img
+          src="/goldfish.png" 
+          alt="Fish Icon"
+          className="w-10 h-10 -ml-12 mr-2 rounded-full"
         />
       )}
+      <div className="block w-full rounded-md border-0 px-3.5 py-2 bg-white text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 sm:text-sm sm:leading-6">
+        {isQuestion ? (
+          <>{text}</>
+        ) : (
+          <TypeAnimation
+            ref={ref}
+            sequence={[`${text}`, () => setLoading(false)]}
+            speed={80}
+            style={{ whiteSpace: "pre-line" }}
+            cursor={false}
+          />
+        )}
+      </div>
     </div>
   );
 }
