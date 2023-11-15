d25b280ed49fb18e917e9d5f019ff8ff6904f2c9
Sharon <sharon.m0626@gmail.com>
2023-11-14 22:34:05 -0800

    Fix package json

--- src/app/webrtc/components/socketInitializer.tsx
+++ src/app/webrtc/components/socketInitializer.tsx
@@ -1,39 +1,33 @@
-'use client'
+"use client";
 import { useEffect, useState } from "react";
 import * as socketio from "socket.io-client";
 export default function SocketInitializer() {
+  useEffect(() => {
+    const socket = socketio.io("https://e242-76-78-137-148.ngrok-free.app/");
 
+    socket.on("connect", async () => {
+      console.log("Successfully connected to FishGPT backend!");
+      // create a webRTC offer
+      // const rtcp = new RTCPeerConnection();
+      // const offer = await rtcp.createOffer();
+      // // send this offer to the backend to send to the python client
+      // console.log(offer);
+      // const offer_obj = {
+      //     type: offer.type,
+      //     sdp: offer.sdp
+      // }
+      // socket.emit('clientOffer', offer_obj);
+    });
 
-    useEffect(() => {
-        const socket = socketio.io("http://localhost:5000");
+    socket.on("imageReceive", (data) => {
+      console.log(data);
+    });
 
-        socket.on("connect", async () => {
-            console.log("Successfully connected to FishGPT backend!");
-            // create a webRTC offer
-            // const rtcp = new RTCPeerConnection();
-            // const offer = await rtcp.createOffer();
-            // // send this offer to the backend to send to the python client
-            // console.log(offer);
-            // const offer_obj = {
-            //     type: offer.type,
-            //     sdp: offer.sdp
-            // }
-            // socket.emit('clientOffer', offer_obj);
-        });
+    return () => {
+      // do this to prevent 2x connections
+      socket.disconnect();
+    };
+  }, []);
 
-        socket.on("imageReceive", (data) => {
-            console.log(data);
-        })
-
-        return () => {
-            // do this to prevent 2x connections
-            socket.disconnect();
-        }
-    }, []);
-
-    return (
-        <div>
-            socket component
-        </div>
-    )
+  return <div>socket component</div>;
 }
