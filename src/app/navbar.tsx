04d11ef85246e0abeb63cd957ed16018534e6b8f
Sharon <sharon.m0626@gmail.com>
2023-11-15 21:42:08 -0800

    Add simple navbar

---
+++ src/app/navbar.tsx
@@ -0,0 +1,9 @@
+export default function NavBar() {
+  return (
+    <div className="bg-gray-900 px-4 sm:px-6 lg:px-8">
+      <div className="flex h-12 items-center justify-between">
+        <div className="flex items-center text-white">FishGPT</div>
+      </div>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import SocketInitializer from "./webrtc/components/socketInitializer";
 import { useState } from "react";
 import { AnimatePresence, motion } from "framer-motion";
 
+import NavBar from "./navbar";
 import Textbox from "./textbox";
 import DisplayBox from "./displaybox";
 import ThoughtBubble from "./thoughtbubble";
@@ -14,51 +15,54 @@ export default function Home() {
   const [loading, setLoading] = useState(false);
 
   return (
-    <main className="grid grid-cols-12 h-screen bg-[url('/background.png')] bg-cover bg-center bg-no-repeat">
-      <div className="col-span-8 grid grid-rows-6 bg-green-300">
-        <SocketInitializer />
-        <div className="row-span-2">FishGPT</div>
-      </div>
-      <div className="col-span-4 h-full p-8 bg-red-300 flex items-end">
-        <div className="w-[85%] mx-auto flex flex-col gap-y-4">
-          {question && (
-            <AnimatePresence>
-              <motion.div
-                key={`question-${toggle}`}
-                initial={{ scale: 0.8, opacity: 0 }}
-                animate={{ scale: 1, opacity: 1 }}
-                exit={{ scale: 0.8, opacity: 0 }}
-                transition={{ type: "spring" }}
-              >
-                <DisplayBox text={question} isQuestion={true} />
-              </motion.div>
-            </AnimatePresence>
-          )}
-          {answers && answers[0] && (
-            <AnimatePresence>
-              <motion.div
-                key={`answer-${toggle}`}
-                initial={{ scale: 0.8, opacity: 0 }}
-                animate={{ scale: 1, opacity: 1 }}
-                exit={{ scale: 0.8, opacity: 0 }}
-                transition={{ type: "spring" }}
-              >
-                <DisplayBox text={answers[0]} setLoading={setLoading} />
-              </motion.div>
-            </AnimatePresence>
-          )}
-          <Textbox
-            setQuestion={setQuestion}
-            setAnswers={setAnswers}
-            setToggle={setToggle}
-            loading={loading}
-            setLoading={setLoading}
-          />
+    <main className="h-screen">
+      <NavBar />
+      <div className="grid grid-cols-12">
+        <div className="col-span-8 grid grid-rows-6 bg-blue-300">
+          <SocketInitializer />
+          <div className="row-span-2">FishGPT</div>
         </div>
-      </div>
-      {/* <div>
+        <div className="col-span-4 h-full p-8 bg-pink-300 flex items-end">
+          <div className="w-[85%] mx-auto flex flex-col gap-y-4">
+            {question && (
+              <AnimatePresence>
+                <motion.div
+                  key={`question-${toggle}`}
+                  initial={{ scale: 0.8, opacity: 0 }}
+                  animate={{ scale: 1, opacity: 1 }}
+                  exit={{ scale: 0.8, opacity: 0 }}
+                  transition={{ type: "spring" }}
+                >
+                  <DisplayBox text={question} isQuestion={true} />
+                </motion.div>
+              </AnimatePresence>
+            )}
+            {answers && answers[0] && (
+              <AnimatePresence>
+                <motion.div
+                  key={`answer-${toggle}`}
+                  initial={{ scale: 0.8, opacity: 0 }}
+                  animate={{ scale: 1, opacity: 1 }}
+                  exit={{ scale: 0.8, opacity: 0 }}
+                  transition={{ type: "spring" }}
+                >
+                  <DisplayBox text={answers[0]} setLoading={setLoading} />
+                </motion.div>
+              </AnimatePresence>
+            )}
+            <Textbox
+              setQuestion={setQuestion}
+              setAnswers={setAnswers}
+              setToggle={setToggle}
+              loading={loading}
+              setLoading={setLoading}
+            />
+          </div>
+        </div>
+        {/* <div>
         <ThoughtBubble text="hello, i am a thought bubble"></ThoughtBubble>
       </div> */}
+      </div>
     </main>
   );
 }
