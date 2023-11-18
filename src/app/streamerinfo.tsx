9ab3e8730e71f5c855f40b2967a9a5d811213d0f
Sharon <sharon.m0626@gmail.com>
2023-11-17 16:08:54 -0800

    Format viewer count

--- src/app/streamerinfo.tsx
+++ src/app/streamerinfo.tsx
@@ -11,38 +11,36 @@ const StreamerInfo: React.FC<StreamerInfoProps> = ({
   activity,
   status,
   profilePic,
-  viewers
+  viewers,
 }) => {
   return (
-    <div className="bg-gray-800 text-white p-4 flex items-start space-x-4">
-      <div className="relative">
-        <img
-          src={profilePic}
-          alt={`${username}'s profile`}
-          className="w-16 h-16 rounded-full"
-        />
-        <div
-          className={`absolute bottom-0 left-1/2 transform -translate-x-1/2  text-xs rounded-full px-2 py-0 ${
-            status === "LIVE" ? "bg-red-500" : "bg-gray-500"
-          }`}
-        >
-          {status}
+    <div className="bg-gray-800 text-white p-4 pr-6 w-full flex justify-between">
+      <div className="flex items-start space-x-4">
+        <div className="relative">
+          <img
+            src={profilePic}
+            alt={`${username}'s profile`}
+            className="w-16 h-16 rounded-full"
+          />
+          <div
+            className={`absolute bottom-0 left-1/2 transform -translate-x-1/2  text-xs rounded-full px-2 py-0 ${
+              status === "LIVE" ? "bg-red-500" : "bg-gray-500"
+            }`}
+          >
+            {status}
+          </div>
+        </div>
+        <div>
+          <div className="text-lg font-bold">{username}</div>
+          <div className="text-md">{activity}</div>
         </div>
       </div>
-      <div>
-        <div className="text-lg font-bold">{username}</div>
-        <div className="text-md">{activity}</div>
-        {status === 'LIVE' && (
-          <div className="text-sm flex items-center">
-            <span className="mr-1">{viewers} viewers</span>
-            <img
-              src="/viewers.png" // Replace with your human icon image path
-              alt="Viewers"
-              className="w-4 h-4"
-            />
-          </div>
-        )}
-      </div>
+      {status === "LIVE" && (
+        <div className="text-md flex items-center gap-x-2">
+          <img src="/viewers.png" alt="Viewers" className="w-6 h-6" />
+          <span className="mr-1">{viewers}</span>
+        </div>
+      )}
     </div>
   );
 };
